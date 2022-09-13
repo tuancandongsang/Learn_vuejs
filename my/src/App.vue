241b21c769039c6381ad2c8fd4ff034ba5095fb4
tuancan <tuacandongsang@gmail.com>
2022-09-13 23:54:54 +0700

    13.9

---
+++ my/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ul>
+      <li>V-for</li>
+      <li>Watch</li>
+      <li>tuan</li>
+
+    </ul>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ul>
+      <h1>Loop array</h1>
+      <li v-for="(item, index) of items" :key="index">
+        {{ index }} - {{ item.name }}
+      </li>
+    </ul>
+
+    <ul >
+      <h1>Loop Object</h1>
+      <li v-for="(value, key) in object" :key="key">
+        {{key}} : {{ value }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      items: [{ name: "Cà phê" }, { name: "Trà đặc" }, { name: "Bò húc" }],
+      object: {
+        // tất nhiên chúng ta đều biết ông Bành Tổ không phải
+        // họ Bành tên Tổ, nhưng đây chỉ là một ví dụ…
+        age: 20,
+        name: "henrry",
+        add: "Hai Duong",
+      },
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
